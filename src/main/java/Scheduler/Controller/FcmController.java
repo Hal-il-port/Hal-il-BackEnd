d25ecb18b0bd409e56146419c62552c5260c8839
wonjong <won72881@naver.com>
2025-08-01 21:01:27 +0900

    8/1 - FCM config, 알림 스웨거 추가

--- src/main/java/Scheduler/Controller/FcmController.java
+++ src/main/java/Scheduler/Controller/FcmController.java
@@ -1,6 +1,9 @@
 package Scheduler.Controller;
 
 import Scheduler.Service.FcmService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,16 +18,27 @@ import java.util.Map;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/fcm")
+@Tag(name = "FCM API", description = "Firebase 클라우드 메시징 관련 API")
 public class FcmController {
 
     private final FcmService fcmService;
+
+    @Operation(
+            summary = "FCM 토큰 등록",
+            description = "사용자 Firebase 클라이언트 토큰을 등록합니다.",
+            responses = {
+                    @ApiResponse(responseCode = "200", description = "토큰 등록 성공"),
+                    @ApiResponse(responseCode = "400", description = "잘못된 요청")
+            }
+    )
 
     // 클라이언트에서 토큰 등록
     @PostMapping("/token")
     public ResponseEntity<Void> registerToken(
             @AuthenticationPrincipal UserDetails userDetails,
             @RequestBody Map<String, String> request
-    ) {
+    )
+    {
         Long userId = Long.parseLong(userDetails.getUsername()); // JWT에서 가져온 유저 ID
         String token = request.get("token");
         fcmService.saveToken(userId, token);
